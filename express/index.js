9dce7e892e795dcfb29e3eb65317872b2a1fae49
wcho <wcho.dev@gmail.com>
2022-10-05 15:52:20 +0900

    feat: add menu and fake pages (#6)

--- express/index.js
+++ express/index.js
@@ -10,4 +10,21 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/leaderboard', (req, res) => {
+  res.render('leaderboard');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/signin', (req, res) => {
+  res.render('signin');
+});
+
+app.get('/day/:num', (req, res) => {
+  const num = req.params.num ? req.params.num : 0;
+  res.render('day', { num });
+});
+
 module.exports = app;
