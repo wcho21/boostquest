188ea9f0da8144ddc91503eb8ee01f23a5c6b1f5
wcho <wcho.dev@gmail.com>
2022-10-05 13:54:22 +0900

    feat: serve index page and favicon as static files (#4, #5)
    Using pug template engine.

--- express/index.js
+++ express/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('static'));
+app.set('views', './pug');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.render('index');
 });
 
 module.exports = app;
