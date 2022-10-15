4b78b2dce2ddd89ec4dc077ba5a3b5f597029a13
wcho <wcho.dev@gmail.com>
2022-10-15 23:55:28 +0900

    feat: reduce sign-in steps (#58)
    Instead of showing sign-in menu, redirect directly to GitHub OAuth sign-in page.

--- routers/signin/index.js
+++ routers/signin/index.js
@@ -3,9 +3,11 @@ const config = require('#config');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  // redirect directly to GitHub OAuth sign-in page
   const clientId = config.oauth.github.clientId;
+  const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}`;
 
-  res.render('signin', { clientId });
+  res.redirect(redirectUrl);
 });
 
 module.exports = router;
