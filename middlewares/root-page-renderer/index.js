0ee0c6bb5f4d683ad18356515a77c0fc44658283
wcho <wcho.dev@gmail.com>
2022-10-16 11:35:56 +0900

    feat: show remaining time for each day (#68)

--- middlewares/root-page-renderer/index.js
+++ middlewares/root-page-renderer/index.js
@@ -1,9 +1,12 @@
+const dayjs = require('dayjs');
+const config = require('#config');
 const problems = require('#database/problems');
 
 module.exports = async (req, res) => {
+  const numOfDays = 4;
+
   res.locals.problemsSolved = [false, false, false, false];
   if (res.locals.signedIn) {
-    const numOfDays = 4;
     const userId = res.locals.user.id;
     const problemIds = [12, 22, 32, 42];
 
@@ -16,6 +19,10 @@ module.exports = async (req, res) => {
       }
     }
   }
+
+  const firstDayOpenTime = dayjs(config.site.firstDayOpenTime);
+  res.locals.openTimeForDay = Array(numOfDays).fill(null)
+    .map((_, i) => dayjs(firstDayOpenTime).add(i, 'day').valueOf());
 
   res.render('root');
 };
